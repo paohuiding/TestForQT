fe33918a110364a4c67c3b313e3caf65d09381b8
paohuiding <1814907127@qq.com>
2022-08-21 22:36:57 +0800

    2222

--- widget.cpp
+++ widget.cpp
@@ -1,15 +1,30 @@
+#define PI 3.14
+
 #include "widget.h"
 #include "ui_widget.h"
+#include<QDebug>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    connect(ui->CountButton,SIGNAL(clicked()),this,SLOT(on_CountButton_clicked()));
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::on_CountButton_clicked()
+{
+    qDebug("111111");
+    bool bOK;
+    QString sRadius;
+    QString sRadiusValue=ui->RadiusLineLabel->text();
+    int iRadiusValue=sRadiusValue.toInt(&bOK);
+    double dArea=iRadiusValue*iRadiusValue*PI;
+    ui->AreaLabel->setText(sRadius.setNum(dArea));
 }
 
--- widget.h
+++ widget.h
@@ -15,6 +15,9 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_CountButton_clicked();
+
 private:
     Ui::Widget *ui;
 };
